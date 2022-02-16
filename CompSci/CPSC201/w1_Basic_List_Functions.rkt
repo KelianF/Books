b834b1327886e50e730f85276ec5ec8a5e792ee4
Kelian <kelianferhat@gmail.com>
2022-02-16 16:04:12 +0900

    W2_bis

--- CompSci/CPSC201/w1_Basic_List_Functions.rkt
+++ CompSci/CPSC201/w1_Basic_List_Functions.rkt
@@ -22,13 +22,47 @@
 ; (cadr '(1 2 3)) -> 2
 
 ; last
+; (last '(1 2 3 4)) -> 4
+
 ; reverse
+; (reverse '(1 2 3 4)) -> '(4 3 2 1)
 
 ; cons
+; takes 2 args, first what to add, second what to add it to
+; it is basically append @ begning for the first place
+; (cons 1 '(2 3 4)) -> '(1 2 3 4)
+
 
 ; append
+; it appends 2 lists; only works with lists
+; (append '(1 2) '(3 4)) -> '(1 2 3 4)
 
 ; map
+; takes in a procedure and a list and applies that procedure to every element in the list
+; output is list
+; (map string? '(1 2 3 "hello" 4 5 'symbol "Good bye")) -> '(#f #f #f #t #f #f #f #t)
+
+; If want to use a function with 2 args, then need to use a lambda
+; (map (lambda (x) (+ 1 x)) '(1 2 3)) -> '(2 3 4)
 
 ; length
+; takes a list
+; outputs an integer representing the number of elements in your list
+; (length '(1 2 3)) -> 3
+
+; empty?
+; takes a list
+; returns #t if list is empty,
+; returns #f if list is not.
+; (empty? '()) -> #t
+; (empty? '(1 2 3)) -> #f
+# (empty? empty) -> #t
+
+
+
+
+
+
+
+
 
